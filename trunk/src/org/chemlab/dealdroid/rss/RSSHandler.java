51765e05a25d972a5d83ff77c07a645c60b4e1eb
steve.kondik <steve.kondik@17770448-fbae-11dd-802b-177316e4ac06>
2009-02-17 03:09:09 +0000

    Make the RSSHandler deal with fragments.  Return all values since we can't depend that they will come in order. Add timestamp comparisons.

--- trunk/src/org/chemlab/dealdroid/rss/RSSHandler.java
+++ trunk/src/org/chemlab/dealdroid/rss/RSSHandler.java
@@ -1,5 +1,12 @@
 package org.chemlab.dealdroid.rss;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 import org.chemlab.dealdroid.Item;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -9,7 +16,7 @@ import android.net.Uri;
 
 /**
  * SAX event handler which has some idea of the kind of RSS that we are interested in,
- * and creates and Item object out of the most recent entry in the feed.
+ * and creates a sorted map of Item objects.
  * 
  * @author shade
  * @version $Id$
@@ -17,20 +24,20 @@ import android.net.Uri;
 public class RSSHandler extends DefaultHandler {
 
 	private enum ItemTag {
-		TITLE, LINK, DESCRIPTION, PRICE;
+		TITLE, LINK, DESCRIPTION, PRICE, PUBDATE;
 	}
 
 	private boolean inItem = false;
 
-	private boolean firstItemComplete = false;
-
 	private ItemTag currentTag = null;
 
-	private final Item currentItem;
+	private Item currentItem;
 
-	public RSSHandler(final Item currentItem) {
-		this.currentItem = currentItem;
-	}
+	private StringBuilder currentString;
+	
+	private final DateFormat pubDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
+	
+	private final SortedMap<Date, Item> items = new TreeMap<Date, Item>();
 
 	/*
 	 * (non-Javadoc)
@@ -41,22 +48,25 @@ public class RSSHandler extends DefaultHandler {
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 
-		if (!firstItemComplete) {
-			if (localName.trim().equals("item")) {
-				inItem = true;
-			} else if (localName.trim().equals("title")) {
-				currentTag = ItemTag.TITLE;
-			} else if (localName.trim().equals("link")) {
-				currentTag = ItemTag.LINK;
-			} else if (localName.trim().equals("listDescription")) {
-				currentTag = ItemTag.DESCRIPTION;
-			} else if (localName.trim().equals("price")) {
-				currentTag = ItemTag.PRICE;
-			} else {
-				currentTag = null;
-			}
-
+		currentString = new StringBuilder();
+		
+		if (localName.trim().equals("item")) {
+			inItem = true;
+			currentItem = new Item();
+		} else if (localName.trim().equals("title")) {
+			currentTag = ItemTag.TITLE;
+		} else if (localName.trim().equals("link")) {
+			currentTag = ItemTag.LINK;
+		} else if (localName.trim().equals("listDescription")) {
+			currentTag = ItemTag.DESCRIPTION;
+		} else if (localName.trim().equals("price")) {
+			currentTag = ItemTag.PRICE;
+		} else if (localName.trim().equals("pubDate")) {
+			currentTag = ItemTag.PUBDATE;
+		} else {
+			currentTag = null;
 		}
+
 	}
 
 	/*
@@ -68,26 +78,19 @@ public class RSSHandler extends DefaultHandler {
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
 
-		if (!firstItemComplete && localName.trim().equals("item")) {
+		if (localName.trim().equals("item")) {
 			inItem = false;
-			firstItemComplete = true;
-			currentTag = null;
-		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.xml.sax.helpers.DefaultHandler#characters(char[], int, int)
-	 */
-	@Override
-	public void characters(char[] ch, int start, int length) throws SAXException {
-
-		if (inItem && currentTag != null) {
-
-			final String chars = (new String(ch).substring(start, start + length)).trim();
-			if (chars.length() > 0) {
-
+			if (currentItem.getPubDate() != null) {
+				final Item clone = (Item)currentItem.clone();
+				items.put(clone.getPubDate(), clone);
+			}
+			
+		} else {
+			
+			if (currentItem != null && currentTag != null) {
+				
+				final String chars = currentString.toString();
+			
 				switch (currentTag) {
 				case TITLE:
 					currentItem.setTitle(chars);
@@ -101,10 +104,44 @@ public class RSSHandler extends DefaultHandler {
 				case PRICE:
 					currentItem.setPrice(chars);
 					break;
+				case PUBDATE:
+					try {
+						currentItem.setPubDate(pubDateFormat.parse(chars));
+					} catch (ParseException e) {
+						throw new SAXException(e);
+					}
+					break;
 				}
-
 			}
 		}
+		
+		currentTag = null;
+		
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.xml.sax.helpers.DefaultHandler#characters(char[], int, int)
+	 */
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+
+		if (inItem && currentTag != null) {
+
+			final String chars = (new String(ch).substring(start, start + length)).trim();
+						
+			if (chars.length() > 0) {
+				currentString.append(chars);
+			}
+		}
+	}
+
+	/**
+	 * @return the items
+	 */
+	public SortedMap<Date, Item> getItems() {
+		return items;
 	}
 
 }
