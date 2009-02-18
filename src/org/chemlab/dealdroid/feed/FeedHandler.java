2350e64d45a2e8b90d0952cbd5856e75396f3ddc
steve.kondik <steve.kondik@17770448-fbae-11dd-802b-177316e4ac06>
2009-02-18 03:47:37 +0000

    Hopefully get the wakelocks right this time.

--- src/org/chemlab/dealdroid/feed/FeedHandler.java
+++ src/org/chemlab/dealdroid/feed/FeedHandler.java
@@ -4,6 +4,9 @@ import org.chemlab.dealdroid.Item;
 import org.xml.sax.ContentHandler;
 
 /**
+ * To handle feeds, a SAX ContentHandler is used, which should return
+ * a single Item when it has completed parsing.  
+ * 
  * @author shade
  * @version $Id$
  */
