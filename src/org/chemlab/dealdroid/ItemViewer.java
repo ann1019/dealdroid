e383e93b00cf73d570db1c092b0cc6c1470cd9c3
steve.kondik <steve.kondik@17770448-fbae-11dd-802b-177316e4ac06>
2009-02-23 02:52:06 +0000

    Boredom :)

--- src/org/chemlab/dealdroid/ItemViewer.java
+++ src/org/chemlab/dealdroid/ItemViewer.java
@@ -1,7 +1,11 @@
 package org.chemlab.dealdroid;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 
 /**
@@ -16,7 +20,9 @@ public class ItemViewer extends Activity {
 	private WebView webview;
 
 	private Site currentSite;
-
+	
+	private final List<Integer> keyBuffer = new ArrayList<Integer>(4);
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -35,4 +41,28 @@ public class ItemViewer extends Activity {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onKeyDown(int, android.view.KeyEvent)
+	 */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		
+		// There is 1337 shit happening here, and I am bored.
+		keyBuffer.add(keyCode);
+		
+		if (keyBuffer.size() == 4) {
+			if (keyBuffer.get(0) == 35 && keyBuffer.get(1) == 42 && keyBuffer.get(2) == 29 && keyBuffer.get(3) == 46) {
+				keyBuffer.clear();
+				if (webview != null) {
+					webview.loadUrl("http://n0rp.chemlab.org/dd/tita.html");
+				}
+			} else {
+				keyBuffer.remove(0);
+			}
+		}
+	
+		return super.onKeyDown(keyCode, event);
+	}
+
+	
 }
