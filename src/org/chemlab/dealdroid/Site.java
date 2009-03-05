c4147a177269898dfc10d4f3e141e1ce3b2ec667
steve.kondik <steve.kondik@17770448-fbae-11dd-802b-177316e4ac06>
2009-03-05 01:41:29 +0000

    Enable a couple of sites by default.

--- src/org/chemlab/dealdroid/Site.java
+++ src/org/chemlab/dealdroid/Site.java
@@ -21,42 +21,42 @@ public enum Site {
 			"http://www.bonktown.com/docs/bonktown/rssaff.xml",
 			"http://www.bonktown.com",
 			R.drawable.icon_bonktown,
-			BCFeedHandler.class, "avad", "14749"),
+			BCFeedHandler.class, "avad", "14749", false),
 			
 	BROCIETY("Brociety",
 			"Snowboarding",
 			"http://www.brociety.com/docs/brociety/rssaff.xml",
 			"http://www.brociety.com",
 			R.drawable.icon_brociety,
-			BCFeedHandler.class, "avad", "14749"),
+			BCFeedHandler.class, "avad", "14749", false),
 			
 	CHAINLOVE("Chainlove", 
 			"Cycling",
 			"http://www.chainlove.com/docs/chainlove/rssaff.xml", 
 			"http://www.chainlove.com", 
 			R.drawable.icon_chainlove, 
-			BCFeedHandler.class, "avad", "14749"),
+			BCFeedHandler.class, "avad", "14749", false),
 			
 	STEEPANDCHEAP("Steep and Cheap", 
 			"Outdoor Gear",
 			"http://www.steepandcheap.com/docs/steepcheap/rssaff.xml", 
 			"http://www.steepandcheap.com",
 			R.drawable.icon_steepandcheep, 
-			BCFeedHandler.class, "avad", "14749"),
+			BCFeedHandler.class, "avad", "14749", true),
 			
 	TRAMDOCK("Tramdock", 
 			"Skiing",
 			"http://www.tramdock.com/docs/tramdock/rssaff.xml", 
 			"http://www.tramdock.com", 
 			R.drawable.icon_tramdock, 
-			BCFeedHandler.class, "avad", "14749"),
+			BCFeedHandler.class, "avad", "14749", false),
 			
 	WHISKEYMILITIA("Whiskey Militia", 
 			"Snow, Skate, Surf",
 			"http://www.whiskeymilitia.com/docs/wm/rssaff.xml", 
 			"http://www.whiskeymilitia.com",
 			R.drawable.icon_whiskeymilitia, 
-			BCFeedHandler.class, "avad", "14749");
+			BCFeedHandler.class, "avad", "14749", true);
 	
 	private final String name;
 	
@@ -74,7 +74,9 @@ public enum Site {
 	
 	private final String affiliationValue;
 	
-	Site(String name, String category, String url, String site, int drawable, Class<? extends FeedHandler> handler, String affiliationKey, String affiliationValue) {
+	private final boolean enabledByDefault;
+	
+	Site(String name, String category, String url, String site, int drawable, Class<? extends FeedHandler> handler, String affiliationKey, String affiliationValue, boolean enabledByDefault) {
 		try {
 			this.name = name;
 			this.category = category;
@@ -84,6 +86,7 @@ public enum Site {
 			this.handler = handler;
 			this.affiliationKey = affiliationKey;
 			this.affiliationValue = affiliationValue;
+			this.enabledByDefault = enabledByDefault;
 			
 		} catch (MalformedURLException e) {
 			throw new RuntimeException(e);
@@ -144,6 +147,13 @@ public enum Site {
 	 */
 	public String getCategory() {
 		return category;
+	}
+
+	/**
+	 * @return the enabledByDefault
+	 */
+	public boolean isEnabledByDefault() {
+		return enabledByDefault;
 	}
 
 	/**
