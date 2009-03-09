5101e5a88fb5fcc740561ef4b3bed88704c543fd
steve.kondik <steve.kondik@17770448-fbae-11dd-802b-177316e4ac06>
2009-03-09 00:37:42 +0000

    Copy for tag dealdroid-1.1.1

---
+++ tags/dealdroid-1.1.1/src/org/chemlab/dealdroid/Item.java
@@ -0,0 +1,350 @@
+package org.chemlab.dealdroid;
+
+import java.util.Date;
+
+import android.net.Uri;
+
+/**
+ * Holds information pulled from a source about the current deal.
+ * 
+ * @author shade
+ * @version $Id$
+ */
+public class Item implements Cloneable {
+
+	private String title;
+
+	private String salePrice;
+
+	private String retailPrice;
+
+	private String savings;
+
+	private String description;
+
+	private String shortDescription;
+
+	private Uri link;
+
+	private Uri imageLink;
+
+	private Date expiration;
+
+	private Date timestamp = new Date();
+
+	/**
+	 * 
+	 */
+	public Item() {
+		super();
+	}
+
+
+	/**
+	 * @param title
+	 * @param salePrice
+	 * @param retailPrice
+	 * @param savings
+	 * @param description
+	 * @param shortDescription
+	 * @param link
+	 * @param imageLink
+	 * @param expiration
+	 */
+	public Item(String title, String salePrice, String retailPrice, String savings, String description,
+			String shortDescription, Uri link, Uri imageLink, Date expiration) {
+		super();
+		this.title = title;
+		this.salePrice = salePrice;
+		this.retailPrice = retailPrice;
+		this.savings = savings;
+		this.description = description;
+		this.shortDescription = shortDescription;
+		this.link = link;
+		this.imageLink = imageLink;
+		this.expiration = expiration;
+	}
+
+
+	/**
+	 * @param title
+	 * @param salePrice
+	 * @param retailPrice
+	 * @param savings
+	 * @param description
+	 * @param shortDescription
+	 * @param link
+	 * @param imageLink
+	 * @param expiration
+	 * @param timestamp
+	 */
+	public Item(String title, String salePrice, String retailPrice, String savings, String description,
+			String shortDescription, Uri link, Uri imageLink, Date expiration, Date timestamp) {
+		this(title, salePrice, retailPrice, savings, description, shortDescription, link, imageLink, expiration);
+		this.timestamp = timestamp;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title
+	 *            the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the salePrice
+	 */
+	public String getSalePrice() {
+		return salePrice;
+	}
+
+	/**
+	 * @param salePrice
+	 *            the salePrice to set
+	 */
+	public void setSalePrice(String salePrice) {
+		this.salePrice = salePrice;
+	}
+
+	/**
+	 * @return the retailPrice
+	 */
+	public String getRetailPrice() {
+		return retailPrice;
+	}
+
+	/**
+	 * @param retailPrice
+	 *            the retailPrice to set
+	 */
+	public void setRetailPrice(String retailPrice) {
+		this.retailPrice = retailPrice;
+	}
+
+	/**
+	 * @return the savings
+	 */
+	public String getSavings() {
+		return savings;
+	}
+
+	/**
+	 * @param savings
+	 *            the savings to set
+	 */
+	public void setSavings(String savings) {
+		this.savings = savings;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the link
+	 */
+	public Uri getLink() {
+		return link;
+	}
+
+	/**
+	 * @param link
+	 *            the link to set
+	 */
+	public void setLink(Uri link) {
+		this.link = link;
+	}
+
+	/**
+	 * @return the imageLink
+	 */
+	public Uri getImageLink() {
+		return imageLink;
+	}
+
+	/**
+	 * @param imageLink
+	 *            the imageLink to set
+	 */
+	public void setImageLink(Uri imageLink) {
+		this.imageLink = imageLink;
+	}
+
+	/**
+	 * @return the shortDescription
+	 */
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	/**
+	 * @param shortDescription
+	 *            the shortDescription to set
+	 */
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	/**
+	 * @return the expiration
+	 */
+	public Date getExpiration() {
+		return expiration;
+	}
+
+	/**
+	 * @param expiration
+	 *            the expiration to set
+	 */
+	public void setExpiration(Date expiration) {
+		this.expiration = expiration;
+	}
+
+	/**
+	 * @return the timestamp
+	 */
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	/**
+	 * @param timestamp the timestamp to set
+	 */
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((imageLink == null) ? 0 : imageLink.hashCode());
+		result = prime * result + ((link == null) ? 0 : link.hashCode());
+		result = prime * result + ((retailPrice == null) ? 0 : retailPrice.hashCode());
+		result = prime * result + ((salePrice == null) ? 0 : salePrice.hashCode());
+		result = prime * result + ((savings == null) ? 0 : savings.hashCode());
+		result = prime * result + ((shortDescription == null) ? 0 : shortDescription.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Item other = (Item) obj;
+		if (description == null) {
+			if (other.description != null) {
+				return false;
+			}
+		} else if (!description.equals(other.description)) {
+			return false;
+		}
+		if (imageLink == null) {
+			if (other.imageLink != null) {
+				return false;
+			}
+		} else if (!imageLink.equals(other.imageLink)) {
+			return false;
+		}
+		if (link == null) {
+			if (other.link != null) {
+				return false;
+			}
+		} else if (!link.equals(other.link)) {
+			return false;
+		}
+		if (retailPrice == null) {
+			if (other.retailPrice != null) {
+				return false;
+			}
+		} else if (!retailPrice.equals(other.retailPrice)) {
+			return false;
+		}
+		if (salePrice == null) {
+			if (other.salePrice != null) {
+				return false;
+			}
+		} else if (!salePrice.equals(other.salePrice)) {
+			return false;
+		}
+		if (savings == null) {
+			if (other.savings != null) {
+				return false;
+			}
+		} else if (!savings.equals(other.savings)) {
+			return false;
+		}
+		if (shortDescription == null) {
+			if (other.shortDescription != null) {
+				return false;
+			}
+		} else if (!shortDescription.equals(other.shortDescription)) {
+			return false;
+		}
+		if (title == null) {
+			if (other.title != null) {
+				return false;
+			}
+		} else if (!title.equals(other.title)) {
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#clone()
+	 */
+	@Override
+	public Object clone() {
+
+		final Item clone;
+
+		try {
+			clone = (Item) super.clone();
+		} catch (CloneNotSupportedException e) {
+			throw new InternalError(e.toString());
+		}
+
+		return clone;
+	}
+}
---
+++ tags/dealdroid-1.1.1/src/org/chemlab/dealdroid/feed/RSSHandler.java
@@ -0,0 +1,209 @@
+package org.chemlab.dealdroid.feed;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.chemlab.dealdroid.Item;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import android.net.Uri;
+import android.util.Log;
+
+/**
+ * SAX event handler which has some idea of the kind of RSS that we are
+ * interested in, and creates a sorted map of Item objects.
+ * 
+ * @author shade
+ * @version $Id$
+ */
+public class RSSHandler extends DefaultHandler implements FeedHandler {
+
+	private enum ItemTag {
+		TITLE, LINK, DESCRIPTION, PRICE, PUBDATE, IMAGE_LINK, SHORT_DESCRIPTION, WOOTOFF;
+	}
+
+	private boolean inItem = false;
+
+	private ItemTag currentTag = null;
+
+	private Item currentItem;
+
+	private Date currentItemDate;
+
+	private StringBuilder currentString;
+
+	private final SortedMap<Date, Item> items = new TreeMap<Date, Item>();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.xml.sax.helpers.DefaultHandler#startElement(java.lang.String,
+	 * java.lang.String, java.lang.String, org.xml.sax.Attributes)
+	 */
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+
+		currentString = new StringBuilder();
+		
+		final String tag = localName.trim().toLowerCase(Locale.getDefault());
+		
+		if (tag.equals("item")) {
+			inItem = true;
+			currentItem = new Item();
+		} else if (tag.equals("title")) {
+			currentTag = ItemTag.TITLE;
+		} else if (tag.equals("link")) {
+			currentTag = ItemTag.LINK;
+		} else if (tag.equals("description")) {
+			currentTag = ItemTag.DESCRIPTION;
+		} else if (tag.equals("price")) {
+			currentTag = ItemTag.PRICE;
+		} else if (tag.equals("pubdate")) {
+			currentTag = ItemTag.PUBDATE;
+		} else if (tag.equals("thumnailimage")) {
+			currentTag = ItemTag.IMAGE_LINK;
+		} else if (tag.equals("subtitle")) {
+			currentTag = ItemTag.SHORT_DESCRIPTION;
+		} else if (tag.equals("wootoff")) {
+			currentTag = ItemTag.WOOTOFF;
+		} else {
+			currentTag = null;
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.xml.sax.helpers.DefaultHandler#endElement(java.lang.String,
+	 * java.lang.String, java.lang.String)
+	 */
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+
+		if (inItem && currentItem != null) {
+			if (localName.trim().equals("item")) {
+							
+				inItem = false;
+				if (currentItemDate != null) {
+					final Item clone = (Item) currentItem.clone();
+					items.put((Date) currentItemDate.clone(), clone);
+					currentItemDate = null;
+				}
+
+			} else if (currentTag != null) {
+
+				final String chars = currentString.toString().trim();
+
+				if (chars != null) {
+					switch (currentTag) {
+					case TITLE:
+						currentItem.setTitle(chars);
+						break;
+					case LINK:
+						currentItem.setLink(Uri.parse(chars));
+						break;
+					case IMAGE_LINK:
+						currentItem.setImageLink(Uri.parse(chars));
+						break;
+					case DESCRIPTION:
+						currentItem.setDescription(chars);
+						break;
+					case PRICE:
+						currentItem.setSalePrice(chars);
+						break;
+					case SHORT_DESCRIPTION:
+						currentItem.setShortDescription(chars);
+						break;
+					case WOOTOFF:
+						// if there is no woot-off, force an expiration
+						if (chars.toLowerCase().equals("false")) {
+							final Calendar c = Calendar.getInstance();
+							c.add(Calendar.HOUR_OF_DAY, 1);
+							currentItem.setExpiration(c.getTime());
+						}
+						break;
+					case PUBDATE:
+						try {
+							final DateFormat pubDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
+							currentItemDate = pubDateFormat.parse(chars);
+						} catch (ParseException e) {
+
+							// BC likes to just send "MDT" sometimes as the pubDate
+							Log.e(this.getClass().getSimpleName(), "[data: " + chars + "] " + e.getMessage(), e);
+						}
+						break;
+					}
+				}
+
+			}
+		}
+		currentTag = null;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.xml.sax.helpers.DefaultHandler#characters(char[], int, int)
+	 */
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+
+		if (inItem && currentTag != null) {
+
+			final String chars = (new String(ch).substring(start, start + length));
+
+			if (chars.length() > 0) {
+				currentString.append(chars);
+			}
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.chemlab.dealdroid.feed.FeedHandler#getCurrentItem()
+	 */
+	@Override
+	public Item getCurrentItem() {
+		final Item ret = items.size() == 0 ? null : items.get(items.lastKey());
+		if (ret.getSalePrice() == null) {
+			ret.setSalePrice(searchDescriptionForPrice(currentItem));
+		}
+		return ret;
+	}
+
+	/**
+	 * @param item
+	 * @return
+	 */
+	private static String searchDescriptionForPrice(final Item item) {
+		
+		String price = null;
+		if (item.getDescription() != null) {
+
+			final String[] cleanDesc = item.getDescription().replaceAll("\\<.*?\\>", "").split("\\n"); 
+			for (String line : cleanDesc) {
+				final String[] spp = line.split("Price:");
+				if (spp.length == 2) {
+					final String[] sp = spp[1].split("\\$");
+					if (sp.length == 2) {
+						price = sp[1];
+						break;
+					}
+				}
+			}
+		}
+		
+		return price;
+	}
+}
