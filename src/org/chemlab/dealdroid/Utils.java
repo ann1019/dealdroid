087edf5f9b4eb3d1eec43da36aef8317bebe2417
steve.kondik <steve.kondik@17770448-fbae-11dd-802b-177316e4ac06>
2009-03-10 02:22:03 +0000

    Docs

--- src/org/chemlab/dealdroid/Utils.java
+++ src/org/chemlab/dealdroid/Utils.java
@@ -14,36 +14,43 @@ import java.util.regex.Pattern;
 public class Utils {
 
 	private static final String RFC822_DATE_FORMAT = "EEE, d MMM yyyy HH:mm:ss Z";
-	
+
 	private static final String PRICE_REGEX = "Price.*\\$(\\d+\\.\\d+)";
-	
+
 	private static final String REPLACE_HTML_REGEX = "\\<.*?\\>";
-	
+
 	/**
+	 * Parses an RFC822 formatted date string.
+	 * 
 	 * @param date
-	 * @return
+	 * @return the parsed Date object
 	 * @throws ParseException
 	 */
 	public static Date parseRFC822Date(final String date) throws ParseException {
 		final DateFormat f = new SimpleDateFormat(RFC822_DATE_FORMAT);
 		return f.parse(date);
 	}
-	
+
 	/**
+	 * Formats a Date according to RFC822.
+	 * 
 	 * @param date
-	 * @return
+	 * @return the Date object formatted according to RFC822
 	 */
 	public static String formatRFC822Date(final Date date) {
 		final DateFormat f = new SimpleDateFormat(RFC822_DATE_FORMAT);
 		return f.format(date);
 	}
-	
+
 	/**
-	 * @param item
-	 * @return
+	 * Attempts to extract the price of an item from a long description of it
+	 * using a regex and HTML stripping.
+	 * 
+	 * @param text
+	 * @return the price of the item
 	 */
 	public static String searchForPrice(final String text) {
-		
+
 		String price = null;
 		if (text != null) {
 
@@ -56,7 +63,7 @@ public class Utils {
 				}
 			}
 		}
-		
+
 		return price;
 	}
 }
